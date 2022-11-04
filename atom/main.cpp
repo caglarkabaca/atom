8ad6dcc9580fb747c927387f6a2bc7b60832bd5d
cacarekt <cacarekt@yandex.com>
2022-11-04 23:24:34 +0300

    testt

--- atom/main.cpp
+++ atom/main.cpp
@@ -1,8 +1,12 @@
 #include <SDL.h>
 #include <iostream>
 
+#include "Test.h"
+
 int main(int, char* []) {
 	SDL_Init(SDL_INIT_VIDEO);
+
+	Test test;
 
 	SDL_Window* window = SDL_CreateWindow("Title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
 
